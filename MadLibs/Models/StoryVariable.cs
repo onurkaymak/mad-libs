0469889994cedde96b104a1d3c65b2fb5e2290aa
onurkaymak <onurkaymak@outlook.com>
2023-09-25 13:33:30 -0700

    create StoryVariable model class

---
+++ MadLibs/Models/StoryVariable.cs
@@ -0,0 +1,20 @@
+namespace MidLabs.Models
+{
+  public class StoryVariable
+  {
+    public string Holiday { get; set; }
+    public string Name1 { get; set; }
+    public string Name2 { get; set; }
+    public string Verb1 { get; set; }
+    public string Verb2 { get; set; }
+    public string Verb3 { get; set; }
+    public string Adjective1 { get; set; }
+    public string Adjective2 { get; set; }
+    public string Adjective3 { get; set; }
+    public string Noun1 { get; set; }
+    public string Noun2 { get; set; }
+    public string Noun3 { get; set; }
+    public string Store1 { get; set; }
+    public string Store2 { get; set; }
+  }
+}
